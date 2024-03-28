0864014c2008cfcf946a7f9ea4a16e71d483a886
Nguyen Van Thinh <54620666+thinhntt@users.noreply.github.com>
2024-03-29 00:44:42 +0700

    Fixed

--- app.py
+++ app.py
@@ -6,24 +6,13 @@ app = Flask(__name__)
 def remove_values(json_obj, values_to_remove):
     if isinstance(json_obj, dict):
         for key, value in list(json_obj.items()):
-            if key in values_to_remove or value in values_to_remove:
+            if value in values_to_remove:
                 del json_obj[key]
             elif isinstance(value, dict) or isinstance(value, list):
                 remove_values(value, values_to_remove)
     elif isinstance(json_obj, list):
         for item in json_obj:
             remove_values(item, values_to_remove)
-
-def remove_keys(json_obj, keys_to_remove):
-    if isinstance(json_obj, dict):
-        for key in keys_to_remove:
-            if key in json_obj:
-                del json_obj[key]
-        for key, value in json_obj.items():
-            remove_keys(value, keys_to_remove)
-    elif isinstance(json_obj, list):
-        for item in json_obj:
-            remove_keys(item, keys_to_remove)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -45,6 +34,7 @@ def index():
         return render_template('index.html', output=output_json, remove_list=remove_list_str, input_json=input_json_str)
     
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
